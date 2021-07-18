46dc72eba6f569ac9e50a504af011f4b9ad7f788
Samueelx <gsammygitau@gmail.com>
2021-07-18 03:26:03 +0300

    fix some few bugs

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 function collectInput(){
     let name = document.getElementById("name").value;
-    let gender = document.querySelector('input[name=gender]:checked').value
+    let gender = document.querySelector('input[name=male-female]:checked').value
     alert("I am " + name + " and I'm " + gender);
 }
