f0d800242519e4236efda2919d8a68c9af3241c8
Samueelx <gsammygitau@gmail.com>
2021-07-19 08:54:36 +0300

    some code refactoring

--- js/script.js
+++ js/script.js
@@ -3,16 +3,12 @@ function collectInput(){
     let gender = document.querySelector('input[name=male-female]:checked').value;
     let dob = document.getElementById("dob").value;
     let parameter = dob.split("");
-    /**dob is a string value.
-     * YYYY-MM-DD
-     * parameter is the array version of the string input
-    */
-     alert("You were born on day " + calculateDay(parameter) + " of the week and your akanname is: " + determineName());
+    output();
 }
 
 function calculateDay(dob){
     /**Day of the week (d) = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) mod 7 */
-    let cc = parseInt(dob[0] + dob[1]); //century digits
+    let cc = parseInt(dob[0] + dob[1]);
     let century = ((cc/4) - 2*cc-1);
     let yy = parseInt(dob[2] + dob[3]);
     let year = (5*yy/4);
@@ -37,4 +33,9 @@ function determineName(){
         akanName = femaleNames[day];
     }
     return akanName;
+}
+function output() {
+    document.getElementById("h3-output").innerHTML = "Voilà!";
+    document.getElementById("p-output").innerHTML = "You were born on " + calculateDay(day) 
+    + " and therefore your Akan Name is: " + determineName();
 }
