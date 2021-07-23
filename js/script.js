d43d594a63899d67ed57144159cc261a410708e5
Samueelx <gsammygitau@gmail.com>
2021-07-23 06:12:17 +0300

    add akan-name generator

--- js/script.js
+++ js/script.js
@@ -19,6 +19,15 @@ function findBirthDay(dob){
     let yy = parseInt(yearString);
 
     //Day of the week (d) = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) mod 7
-    let day = (((cc/4) - 2*cc-1) + (5*yy/4) + (26*(dateArray[1] + 1)/10) + dateArray[2]) % 7;
+    let day = Math.floor((((cc/4) - 2*cc-1) + (5*yy/4) + (26*(dateArray[1] + 1)/10) + dateArray[2]) % 7);
     //return date;
+}
+
+function generateAkanName(){
+    let gender = document.querySelector('input[name=male-female]:checked').value;
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
+    let maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+    let femaleNames = ["Akosua", "Adwoa", " Abenaa", " Akua", "Yaa", "Afua", " Ama"];
+
+    
 }
